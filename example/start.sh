90bbe603b3910a65fc934e432a26fb7c7f38a2dd
Dániel Sipos <dsipos@szintezis-net.hu>
2021-11-01 11:45:50 +0100

    Ugrade local dependency path

--- example/start.sh
+++ example/start.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 
 # sudo npm i -g expo-cli expo-optimize
-rm -rf node_modules/@siposdani87
 npm i
+rm -rf node_modules/@siposdani87
+mkdir -p node_modules/@siposdani87/expo-rich-text-editor
+cp -R ../dist node_modules/@siposdani87/expo-rich-text-editor
+cp -R ../package.json node_modules/@siposdani87/expo-rich-text-editor
 npm run tsc-test 
 expo start -c
