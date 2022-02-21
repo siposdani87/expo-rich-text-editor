f1da429475082d8f360d8b34461c688e9f7259ab
Dániel Sipos <siposdani87@hotmail.com>
2022-02-21 23:48:41 +0100

    Fix type of editorStyle

--- src/RichTextViewer.tsx
+++ src/RichTextViewer.tsx
@@ -1,11 +1,11 @@
 import React, { useCallback } from 'react';
-import { StyleProp, TextStyle, ViewStyle } from 'react-native';
+import { StyleProp, TextStyle } from 'react-native';
 import RichTextEditor from './RichTextEditor';
 
 export default function RichTextViewer(props: {
     value: string;
     linkStyle?: StyleProp<TextStyle>;
-    editorStyle?: StyleProp<ViewStyle>;
+    editorStyle?: StyleProp<TextStyle>;
     debug?: boolean;
 }) {
     const onValueChange = useCallback(() => {
