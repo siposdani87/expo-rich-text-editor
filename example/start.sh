39f779b5cd716f76b3b197a35633721ed960e734
Dániel Sipos <siposdani87@hotmail.com>
2022-09-18 22:04:02 +0200

    Upgrade packages

--- example/start.sh
+++ example/start.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
 
-npm i
 rm -rf node_modules/@siposdani87
 mkdir -p node_modules/@siposdani87/expo-rich-text-editor
 cp -R ../dist node_modules/@siposdani87/expo-rich-text-editor
 cp -R ../package.json node_modules/@siposdani87/expo-rich-text-editor
 npm run tsc-test 
-expo start -c
+npm run start
